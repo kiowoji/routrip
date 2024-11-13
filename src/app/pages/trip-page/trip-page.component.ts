7c1cf588ead1fc69c44637de6f76027c45ee08f6
Iryna Murzak <iramurzak@gmail.com>
2024-11-13 09:32:50 +0200

    implement trip deletion

--- src/app/pages/trip-page/trip-page.component.ts
+++ src/app/pages/trip-page/trip-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TripService } from 'src/app/services/trip.service';
 import { Trip } from 'src/app/interfaces/trip';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -12,18 +12,53 @@ import { Observable } from 'rxjs';
 export class TripPageComponent implements OnInit {
   panelOpenState = false;
   trip$: Observable<Trip> | undefined;
+  showModal: boolean = false;
+  currentImageIndex: number = 0; 
+  tripImages: string[] = []; 
+  currentImage: string = ''; 
 
   constructor(
     private route: ActivatedRoute,
-    private tripService: TripService
+    private tripService: TripService,
+    private router: Router 
   ) {}
 
   ngOnInit(): void {
-    // Get the trip ID from the URL
     const tripId = this.route.snapshot.paramMap.get('id');
     if (tripId) {
-      // Fetch the trip data from the service
       this.trip$ = this.tripService.getTripById(tripId);
+      this.trip$.subscribe(trip => {
+        if (trip.images && trip.images.length > 0) {
+          this.tripImages = trip.images;
+          this.currentImage = this.tripImages[this.currentImageIndex]; 
+        }
+      });
+    }
+  }
+
+  changeImage(direction: number): void {
+    if (this.tripImages.length > 0) {
+      this.currentImageIndex = (this.currentImageIndex + direction + this.tripImages.length) % this.tripImages.length;
+      this.currentImage = this.tripImages[this.currentImageIndex];
+    }
+  }
+
+  showDeleteModal() {
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
+  }
+
+  deleteTrip() {
+    const tripId = this.route.snapshot.paramMap.get('id');
+    if (tripId) {
+      this.tripService.deleteTrip(tripId).then(() => {
+        this.router.navigate(['/all-trips']);
+      }).catch((error) => {
+        console.error("Error deleting trip:", error);
+      });
     }
   }
 }
